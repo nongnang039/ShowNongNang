7323feb828c14a8c23e4d55a5ac1f230c8b5e389
test <nang2539@gmail.com>
2018-04-25 14:45:21 +0700

    test

---
+++ app/src/main/java/com/example/rakthida/shownongnang/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.rakthida.shownongnang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rakthida.shownongnang.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//       Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+
+
+
+
+    }   // Main Method
+
+}   // Main Class
